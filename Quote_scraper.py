efc66c8eb03db8d05ef6ee3bb794b6fc1ac13b0e
rayyy005 <rayankhann005@gmail.com>
2025-05-13 00:40:44 +0500

    Added user input for different tags of quotes they might want

--- Quote_scraper.py
+++ Quote_scraper.py
@@ -7,28 +7,30 @@ from bs4 import BeautifulSoup
 url = 'https://quotes.toscrape.com/'
 response = requests.get(url)
 soup = BeautifulSoup(response.text , 'html.parser')
+
+#This looks for the class 'tag' which is stored in a tag and then we are getting the text of the tags
 tags = soup.find_all('a', class_='tag')
 
-#In here we are looking for the inpirational tag in a class of tags 'a' and then we are getting the href link of the inspirational tag
 
+user_input = input("Enter the tag you want to scrape: ")
 for tag in tags :
-    if tag.text.strip().lower() == 'inspirational':
+    if tag.text.strip().lower() == user_input.strip().lower():
 
         #The href link is relative and needs to be combined with the base URL to get the full URL
-        inspirational_url = url + tag['href']
+        user_url = url + tag['href']
 
         #Printing the inspirational URL jusr to check if we are getting the correct URL
-        print("Inspirational URL: ", inspirational_url)
+        print("Inspirational URL: ", user_url)
 
-        #Now we are sending a GET request to the inspirational URL and parsing the response using BeautifulSoup
-        response_inspirational = requests.get(inspirational_url)
+        #Now we are sending a Get request to the user URL and parsing the response using BeautifulSoup
+        response_user = requests.get(user_url)
 
         #Parsing the response using BeautifulSoup
-        soup_inspirational = BeautifulSoup(response_inspirational.text , 'html.parser')
+        soup_user = BeautifulSoup(response_user.text , 'html.parser')
 
         #This looks for the class 'text' which is stored in a span tag and then we are getting the text of the quotes
 
-        quotes = soup_inspirational.find_all('span', class_='text')
+        quotes = soup_user.find_all('span', class_='text')
 
         #This loops through the quotes and prints them out
         for quote in quotes:
