96cddd41f12a808c73b3e5a2348615fb6b42ddc7
KhanBuilds <188497684+KhanBuilds@users.noreply.github.com>
2025-08-18 01:28:04 +0500

    updated

--- news.py
+++ news.py
@@ -1,5 +1,5 @@
 import requests
-API_KEY = "e3ZA9tEzJG3yhcLHPw6kVzKKZ1VgW7VCzPWQlE4C" #bro stop looking for api keys BE BETTER
+API_KEY = "APIKEY" #bro stop looking for api keys BE BETTER
 
 
 news_url = f"https://api.thenewsapi.com/v1/news/top?api_token={API_KEY}&locale=us&limit=3&search=apple-inc"
